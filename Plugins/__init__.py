77d31eef18e47490d29934ebead241c02d36a78f
MEYİTZADE <silaatesss.31@gmail.com>
2023-12-26 07:26:52 +0300

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -5,5 +5,5 @@ from Configs import *
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-bot = TelegramClient('Meyit47', api_id=Config.APP_ID, api_hash=Config.API_HASH)
+bot = TelegramClient('Meyit47', api_id=Configs.APP_ID, api_hash=Configs.API_HASH)
 Meyit47 = bot.start(bot_token=Configs.TOKEN)
